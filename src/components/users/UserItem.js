80d8c39a3bbfd16a8e42b5afd5df0754e5ecad63
Jesse-Tingle <jtingle0@gmail.com>
2019-11-10 01:48:07 -0500

    built UserItem component with state for single user

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
 class UserItem extends Component {
+
+    state = {
+        id: 'id',
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo'
+    }
+
     render() {
+        const {login, avatar_url, html_url } = this.state;
+        
         return (
-            <div>
-                User Item
+            <div className="card text-center">
+                <img src={avatar_url}  alt="" className="round-img" style={{ width: '60px' }} />
+                <h3>{login}</h3>
+
+                <a href={html_url } className="btn btn-dark btn-sm m-1" >More</a>
             </div>
         )
     }
